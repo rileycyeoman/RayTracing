b53f8202684fc276d077247f899eb092859a5331
Riley Yeoman <rileycyeoman@outlook.com>
2025-01-01 06:42:32 -0600

    Sphere drawing

---
+++ src/InOneWeekend/vec3.h
@@ -0,0 +1,102 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <cmath>
+#include <iostream>
+
+class vec3 {
+  public:
+    double e[3];
+
+    vec3() : e{0,0,0} {} //default constructor, all 0s
+    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {} //constructor for initializing values to desired amount
+    
+    //accessor functions
+    double x() const { return e[0]; } 
+    double y() const { return e[1]; }
+    double z() const { return e[2]; }
+
+    //unary operator that negates whole vector if negative is in front
+    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+    //access to each element
+    double operator[](int i) const { return e[i]; } //access, no modification
+    double& operator[](int i) { return e[i]; } //access, modification 
+    //add
+    vec3& operator+=(const vec3& v) {
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        e[2] += v.e[2];
+        return *this;
+    }
+    //multiply
+    vec3& operator*=(double t) {
+        e[0] *= t;
+        e[1] *= t;
+        e[2] *= t;
+        return *this;
+    }
+    //divide
+    vec3& operator/=(double t) {
+        return *this *= 1/t;
+    }
+    //length (euclidean magnitude)
+    double length() const {
+        return std::sqrt(length_squared());
+    }
+    //square of length
+    double length_squared() const {
+        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+    }
+};
+
+// point3 is just an alias for vec3, but useful for geometric clarity in the code.
+using point3 = vec3;
+
+
+// Vector Utility Functions
+
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vec3 operator+(const vec3& u, const vec3& v) {
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline vec3 operator-(const vec3& u, const vec3& v) {
+    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+inline vec3 operator*(const vec3& u, const vec3& v) {
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline vec3 operator*(double t, const vec3& v) {
+    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+}
+
+inline vec3 operator*(const vec3& v, double t) {
+    return t * v;
+}
+
+inline vec3 operator/(const vec3& v, double t) {
+    return (1/t) * v;
+}
+
+inline double dot(const vec3& u, const vec3& v) {
+    return u.e[0] * v.e[0]
+         + u.e[1] * v.e[1]
+         + u.e[2] * v.e[2];
+}
+
+inline vec3 cross(const vec3& u, const vec3& v) {
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+inline vec3 unit_vector(const vec3& v) {
+    return v / v.length();
+}
+
+#endif
