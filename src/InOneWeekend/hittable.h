487d2f8f8f83d8b076b1ae66b8166fa3aa9e2b16
Riley Yeoman <rileycyeoman@outlook.com>
2025-01-06 16:46:09 -0600

    Abstractions

--- src/InOneWeekend/hittable.h
+++ src/InOneWeekend/hittable.h
@@ -1,20 +1,26 @@
 #ifndef HITTABLE_H
 #define HITTABLE_H
 
-#include "ray.h"
 
 class hit_record {
     public:
         point3 p;
         vec3 normal;
         double t;
+        bool front_face;
+
+        void set_face_normal(const ray& r, const vec3& outward_normal){
+            //Sets the normal vecotr for the hit record
+            front_face = dot(r.direction(), outward_normal) < 0; //if opposite directions, ray is coming from outside object
+            normal = front_face ? outward_normal : -outward_normal; //if 
+        }
 };
 
 class hittable{
     public:
         virtual ~hittable() = default;
 
-        virtual bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
+        virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
       
 };
 
